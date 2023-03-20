f0964ee9a2456802bafa27104686ce7378dd88b0
Biljana Novakovic <novakovic1701@gmail.com>
2023-03-20 19:59:53 +0100

    hero left side

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from './components/header/Header';
 import css from './App.css'
+import Header from './components/header/Header';
+import Hero from './components/hero/Hero';
 
 function App() {
   return (
     <div>
      <Header/> 
+     <Hero/>
     </div>
   );
 }
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import css from './Hero.module.css'
+function Hero() {
+  return (
+    <div className={css.container}>
+
+    {/*leftside*/}
+    <div className={css.h_sides}>
+        <span>skin pretection cream</span>
+    
+    <div className={css.text2}>
+        <span>Trendy Collection</span>
+        <span>Seedily say has suitable disposal and boy. Exercise joy man children rejoiced.</span>
+    </div>
+    </div>
+    </div>
+
+    )
+}
+
+export default Hero
