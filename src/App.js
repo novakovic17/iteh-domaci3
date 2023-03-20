9bc2e6c1050a5222fc0b926dc581c2b3f95d38bf
Biljana Novakovic <novakovic1701@gmail.com>
2023-03-20 23:44:25 +0100

    dodavanje swiper-a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import css from './App.css'
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
+import Slider from './components/slider/Slider';
 
 function App() {
   return (
     <div>
      <Header/> 
      <Hero/>
+     <Slider/>
     </div>
   );
 }
---
+++ src/components/slider/Slider.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Swiper, SwiperSlide} from "swiper/react"
+import { Pagination, Navigation } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import {SliderProducts} from '../../data/products'
+
+
+function Slider() {
+  return (
+   
+    <div className="s-container">
+        <Swiper sidesPerView = {3} 
+        spaceBetween = {40} 
+        slidesPerGroup = {1}
+        loop = {true}> 
+            {SliderProducts.map((slide,i) =>
+                <SwiperSlide>
+                </SwiperSlide>
+            )}
+          
+       </Swiper>
+    </div>
+    )
+}
+
+export default Slider
