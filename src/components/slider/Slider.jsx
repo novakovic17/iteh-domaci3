ffd2305f8a775edbe4659d31cd2fd9ea5486555c
Biljana Novakovic <novakovic1701@gmail.com>
2023-03-21 23:02:12 +0100

    skoro zavrsen slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import { Pagination, Navigation} from 'swiper';
-import 'swiper/css';
+import React from 'react'
+import {Swiper, SwiperSlide} from "swiper/react"
+import { Pagination, Navigation } from 'swiper'
+import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
+import {SliderProducts} from '../../data/products'
+import './Slider.css'
 
 const Slider = () => {
   return (
 
     <div className="s-container">
-        <Swiper>
-            <SwiperSlide>1</SwiperSlide>
-            <SwiperSlide>2</SwiperSlide>
-            <SwiperSlide>3</SwiperSlide>
-        </Swiper>
+        <Swiper 
+        modules={[Pagination, Navigation]}
+        className = "mySwiper"
+        navigation = {true}
+        spaceBetween = {40} 
+        slidesPerGroup = {1}
+        loop = {true}> 
+            {SliderProducts.map((slide,i) =>(
+                <SwiperSlide key={i}>
+                    <div className="left-s">
+                        <div className="name">
+                            <span>{slide.name}</span>
+                            <span>{slide.detail}</span>
+                        </div>
+                        <span>{slide.price}$</span>
+                        <div>Shop now</div>
+                    </div>
+                    <img src={slide.img} alt="product" className='img-p' />
+                </SwiperSlide>
+))}
+
+       </Swiper>
     </div>
+    );
+};
 
-    )
-}
-
-export default Slider
+export default Slider;
