3b418255848a8ca2c7aad6260d1a4c03c4aeb3b9
Biljana Novakovic <novakovic1701@gmail.com>
2023-03-21 20:23:54 +0100

    slider pocetak

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,27 +1,21 @@
-import React from 'react'
-import {Swiper, SwiperSlide} from "swiper/react"
-import { Pagination, Navigation } from 'swiper'
-import 'swiper/css'
+import React from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { Pagination, Navigation} from 'swiper';
+import 'swiper/css';
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
-import {SliderProducts} from '../../data/products'
 
-
-function Slider() {
+const Slider = () => {
   return (
-   
+
     <div className="s-container">
-        <Swiper sidesPerView = {3} 
-        spaceBetween = {40} 
-        slidesPerGroup = {1}
-        loop = {true}> 
-            {SliderProducts.map((slide,i) =>
-                <SwiperSlide>
-                </SwiperSlide>
-            )}
-          
-       </Swiper>
+        <Swiper>
+            <SwiperSlide>1</SwiperSlide>
+            <SwiperSlide>2</SwiperSlide>
+            <SwiperSlide>3</SwiperSlide>
+        </Swiper>
     </div>
+
     )
 }
 
