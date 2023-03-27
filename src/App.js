265c6437e173aa194df3fef08f2ff4a153e54fe6
Biljana Novakovic <novakovic1701@gmail.com>
2023-03-27 17:08:38 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
 import Products from './components/products/Products';
@@ -11,6 +12,7 @@ function App() {
      <Hero/>
      <Slider/>
      <Products/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import css from './Footer.module.css';
+import{InboxIcon, PhoneIcon, LocationMarkerIcon, LoginIcon,UsersIcon,LinkIcon} from "@heroicons/react/outline";
+
+
+const Footer = () => {
+  return (
+    <div className={css.cFooterWrapper}>
+        <hr />
+        <div className={css.cFooter}>
+            
+    <div className={css.block}>
+        <div className={css.detail}>
+            <span>Contact us</span>
+            <span class name ={css.pngLine}>
+            <LocationMarkerIcon className = {css.icon}/>
+            <span>111 north avenue Orlando, FL 32801</span>
+            </span>
+            <span className={css.pngLine}>
+            <PhoneIcon className = {css.icon}/>
+            <a href="tel:352-306-4415">352-306-4415</a>
+            </span>
+        </div>
+    </div>
+
+    <div className={css.block}>
+        <div className={css.detail}>
+            <span>Company</span>
+            <span class name = {css.pngLine}>
+            <UsersIcon className = {css.icon}/>
+            <span>About us</span>
+            </span>
+        </div>
+    </div>
+
+    <div className={css.block}>
+        <div className={css.detail}>
+            <span>Resources</span>
+            <span class name = {css.pngLine}>
+            <LinkIcon className = {css.icon}/>
+            <span>Safety Privacy & Terms</span>
+            </span>
+        </div>
+    </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Footer
